a22d966307158d0af689d7818197a8697a068d47
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-01-25 23:55:58 -0500

    Fixed bugs with resizing and changing data

--- src/PieChart.tsx
+++ src/PieChart.tsx
@@ -1,53 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Highcharts from 'highcharts';
+import Highcharts, { Chart } from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { DataPoint } from './DataPoint';
-
-const initialOptions = {
-    chart: {
-        type: 'pie',
-        style: {
-            fontFamily: 'Overpass',
-        },
-    },
-    credits: {
-        enabled: false,
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            dataLabels: {
-                distance: 30, // this is the default
-                format: '{point.name}<br /><b>{point.y}</b>',
-                style: {
-                    fontSize: '12px',
-                    fontWeight: 300,
-                    textOverflow: 'clip',
-                },
-            },
-        },
-        series: {
-            cursor: 'pointer',
-            states: {
-                hover: {
-                    enabled: false,
-                },
-                inactive: {
-                    opacity: 1,
-                },
-            },
-        },
-    },
-    title: {
-        align: 'left',
-        style: {
-            fontSize: '16px',
-        },
-    },
-    tooltip: {
-        enabled: false,
-    },
-};
 
 Highcharts.setOptions({
     colors: [
@@ -77,16 +31,122 @@ export const PieChart = ({
     pieInnerSize,
     data,
 }: PieChartProps) => {
-    const chartRef = useRef(null);
-    const [chartOptions, setChartOptions] = useState(initialOptions);
+    const chartRef = useRef<{
+        chart: Chart;
+        container: React.RefObject<HTMLDivElement>;
+    }>(null);
+
+    const [chartOptions, setChartOptions] = useState({
+        chart: {
+            type: 'pie',
+            style: {
+                fontFamily: 'Overpass',
+            },
+            events: {
+                render() {
+                    const chart = chartRef?.current?.chart;
+                    if (!chart) {
+                        return;
+                    }
+
+                    // destroy old total elements
+                    // @ts-ignore
+                    if (chart.totalElement && chart.totalElement) {
+                        // @ts-ignore
+                        chart.totalElement.destroy();
+                        // @ts-ignore
+                        chart.totalLabelElement.destroy();
+                    }
+
+                    // compute total
+                    const total = data.reduce(
+                        (accumulator, currentValue) =>
+                            accumulator + currentValue.y,
+                        0
+                    );
+
+                    // render total
+                    // @ts-ignore
+                    chart.totalElement = chart.renderer
+                        .text(total.toString(), 0, 0)
+                        .css({
+                            fontSize: '32px',
+                        })
+                        .add();
+
+                    // @ts-ignore
+                    chart.totalLabelElement = chart.renderer
+                        .text(totalLabel, 0, 0)
+                        .css({
+                            fontSize: '12px',
+                        })
+                        .add();
+
+                    // @ts-ignore
+                    const totalElementBox = chart.totalElement.getBBox();
+                    // @ts-ignore
+                    const totalLabelElementBox = chart.totalLabelElement.getBBox();
+
+                    // Place total above center.
+                    // This is done by ignoring totalElementBox.height
+                    // @ts-ignore
+                    chart.totalElement.translate(
+                        chart.plotLeft +
+                            (chart.plotWidth - totalElementBox.width) / 2,
+                        chart.plotTop + chart.plotHeight / 2
+                    );
+
+                    // Place total label below label.
+                    // @ts-ignore
+                    chart.totalLabelElement.translate(
+                        chart.plotLeft +
+                            (chart.plotWidth - totalLabelElementBox.width) / 2,
+                        chart.plotTop +
+                            (chart.plotHeight + totalElementBox.height) / 2
+                    );
+                },
+            },
+        },
+        credits: {
+            enabled: false,
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                dataLabels: {
+                    distance: 30, // this is the default
+                    format: '{point.name}<br /><b>{point.y}</b>',
+                    style: {
+                        fontSize: '12px',
+                        fontWeight: 300,
+                        textOverflow: 'clip',
+                    },
+                },
+            },
+            series: {
+                cursor: 'pointer',
+                states: {
+                    hover: {
+                        enabled: false,
+                    },
+                    inactive: {
+                        opacity: 1,
+                    },
+                },
+            },
+        },
+        title: {
+            align: 'left',
+            style: {
+                fontSize: '16px',
+            },
+        },
+        tooltip: {
+            enabled: false,
+        },
+    });
 
     useEffect(() => {
-        // @ts-ignore
-        const chart = chartRef?.current?.chart;
-        if (!chart) {
-            return;
-        }
-
         // set options from props
         setChartOptions({
             plotOptions: {
@@ -95,66 +155,13 @@ export const PieChart = ({
                     size: pieSize,
                     innerSize: pieInnerSize,
                 },
-                series: {
-                    cursor: 'pointer',
-                    states: {
-                        hover: {
-                            enabled: false,
-                        },
-                        inactive: {
-                            opacity: 1,
-                        },
-                    },
-                },
             },
-            // @ts-ignore
-            series: [
-                {
-                    data,
-                },
-            ],
+            series: [{ data }],
             title: {
                 // @ts-ignore
                 text: title,
             },
         });
-
-        // compute total
-        const total = data.reduce(
-            (accumulator, currentValue) => accumulator + currentValue.y,
-            0
-        );
-
-        // render total
-        const totalElement = chart.renderer
-            .text(total, 0, 0)
-            .css({
-                fontSize: '32px',
-            })
-            .add();
-
-        const totalLabelElement = chart.renderer
-            .text(totalLabel, 0, 0)
-            .css({
-                fontSize: '12px',
-            })
-            .add();
-
-        const totalElementBox = totalElement.getBBox();
-        const totalLabelElementBox = totalLabelElement.getBBox();
-
-        // Place total above center.
-        // This is done by ignoring totalElementBox.height
-        totalElement.translate(
-            chart.plotLeft + (chart.plotWidth - totalElementBox.width) / 2,
-            chart.plotTop + chart.plotHeight / 2
-        );
-
-        // Place total label below label.
-        totalLabelElement.translate(
-            chart.plotLeft + (chart.plotWidth - totalLabelElementBox.width) / 2,
-            chart.plotTop + (chart.plotHeight + totalElementBox.height) / 2
-        );
     }, [title, totalLabel, pieSize, pieInnerSize, data]);
 
     return (
